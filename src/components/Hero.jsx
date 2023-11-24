199d0d873b1ec24a6a9cfa5da8a821d7f85be2c4
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-24 21:01:33 +0200

    Update hero section for tablet version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,24 @@
 import "../styles/hero.css";
 import codeSnippet from "../assets/home/Intro-snippet.png";
+//import blueBlob from "../assets/blobs/blue-haikei.png"
 
 function Hero() {
   return (
     <div className="hero">
-      <div className="hero-wrapper">
-        <div className="hero-body">
-          <h2>Greetings, it&apos;s Salma.</h2>
-          <p>
-            A <span className="role">Frontend Devloper</span> who loves to see
-            white pages turn into something great and user friendly!
-          </p>
-        </div>
+      <div className="hero-container">
+        <div className="hero-wrapper">
+          <div className="hero-body">
+            <h2>Greetings, it&apos;s Salma.</h2>
+            <p>
+              A <span className="role">Frontend Devloper</span> who loves to
+              make blank pages turn into something great and user friendly!
+            </p>
+            
+          </div>
 
-        <div className="code-snippet-wrapper">
-          <img src={codeSnippet} alt="code snippet" />
+          <div className="code-snippet-wrapper">
+            <img src={codeSnippet} alt="code snippet" />
+          </div>
         </div>
       </div>
     </div>
@@ -22,4 +26,3 @@ function Hero() {
 }
 
 export default Hero;
-
