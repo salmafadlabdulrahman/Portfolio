c74c025541b3e4fbdac6fe328f78ea4cca815022
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-24 15:16:14 +0200

    build hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,72 @@
+import "../styles/hero.css";
+import codeSnippet from "../assets/code-snippet.png"
+
 function Hero() {
   return (
-    <div>
-      <h1>Hero section</h1>
+    <div className="hero">
+      <div className="hero-wrapper">
+        <h2>Greetings, it&apos;s Salma.</h2>
+        <p>
+          A <span className="role">Frontend Devloper</span> who loves to see
+          white pages turn out to be something great and user friendly!
+        </p>
+
+        <div className="code-board-section">
+          <div className="code-intro-wrapper">
+            <div className="nav-icons">
+              <span className="red-circle"></span>
+              <span className="yellow-circle"></span>
+              <span className="green-circle"></span>
+            </div>
+
+            <div className="code-section">
+              <img src={codeSnippet} alt="" />
+              {/*<div className="code-body">
+                class Introduction &#123;
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;constructor() &#123;
+                <br />
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.name
+                = &quot;Salma Fadl Azzam&quot;
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.skills =
+                [&quot;HTM&quot;, &quot;CSS&quot;, &quot;JavaScript&quot;, &quot;React&quot;, &quot;Git&quot;];
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.message =
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&quot;I&apos;m
+                Salma, I&apos;m the frontend developer and welcome to the Good Place!!
+                &quot;;
+                <br />
+                &nbsp;&nbsp;&nbsp;&nbsp;&#125;
+                <br />
+                &#125;
+              </div>*/}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
+
+//<img src={codeSnippet} width={500} />
+/*<code class="language-javascript">
+      // Your code goes here
+      function greet() {
+        console.log("Hello, World!");
+      }
+    </code> */
+
+{
+  /*<span>1 let currentLocation = &quot;Egypt, Cairo&quot;</span>*/
+}
+{
+  /*<pre>
+                  const techSkills = [&quot;HTML&quot;, &quot;CSS&quot;, &quot;JavaScript&quot;, &quot;React&quot;, &quot;Git&quot;]
+                  function Introduction() {
+                    
+                  }*/
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+class Introduction {
+  constructor() {
+    this.name = "Salma Fadl Azzam";
+    this.skills = ["HTML", "CSS", "JavaScript", "React", "Git"];
+    this.message =
+      "I'm Salma, i'm the frontend developer and welcome to the Good Place :) ";
+  }
+}
+
+console.log(Introduction);
