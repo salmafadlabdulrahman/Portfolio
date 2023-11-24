cf4408a1d4565f0cc710707e3adbfb26d25b3f78
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-24 11:14:29 +0200

    Initial Commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import "../styles/navbar.css"
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-wrapper">
+        <nav>
+            <span className="logo">Sa</span>
+            <ul>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
