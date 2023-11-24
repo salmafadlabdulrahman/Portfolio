df4aebede08ec41067859b5826e4519f258d789f
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-24 17:58:10 +0200

    style hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import "../styles/hero.css";
-import codeSnippet from "../assets/carbon (5).png";
-
+import codeSnippet from "../assets/17008411212358274.png";
+//carbon (5).png
+//carbon__5_-removebg-preview.png
 function Hero() {
   return (
     <div className="hero">
@@ -9,29 +10,13 @@ function Hero() {
           <h2>Greetings, it&apos;s Salma.</h2>
           <p>
             A <span className="role">Frontend Devloper</span> who loves to see
-            white pages turn out to be something great and user friendly!
+            white pages turn into something great and user friendly!
           </p>
         </div>
 
         <div className="code-snippet-wrapper">
           <img src={codeSnippet} alt="code snippet" />
         </div>
-
-        {/*<div className="code-board-section">
-          <div className="code-intro-wrapper">
-            <div className="nav-icons">
-              <span className="red-circle"></span>
-              <span className="yellow-circle"></span>
-              <span className="green-circle"></span>
-            </div>
-
-            <div className="code-section">
-              <div className="code-body">
-                
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
@@ -39,25 +24,3 @@ function Hero() {
 
 export default Hero;
 
-/*<div className="code-section">
-              <div className="code-body">
-                class <span className="class-title">Introduction</span> &#123;
-                <br />
-                &nbsp;&nbsp;&nbsp;&nbsp;constructor() &#123;
-                <br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.name =
-                &quot;Salma Fadl Azzam&quot;
-                <br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.skills =
-                [&quot;HTM&quot;, &quot;CSS&quot;, &quot;JavaScript&quot;,
-                &quot;React&quot;, &quot;Git&quot;];
-                <br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;this.message =
-                &quot;I&apos;m Salma, I&apos;m the frontend developer and
-                welcome to the Good Place!! &quot;;
-                <br />
-                &nbsp;&nbsp;&nbsp;&nbsp;&#125;
-                <br />
-                &#125;
-              </div>
-            </div> */
