6b802b33acc53861dac342640d2f7dfc294b6835
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-25 16:53:02 +0200

    style the skills section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import "../styles/hero.css";
 import codeSnippet from "../assets/home/Intro-snippet.png";
-//import blueBlob from "../assets/blobs/blue-haikei.png"
 
 function Hero() {
   return (
@@ -10,7 +9,7 @@ function Hero() {
           <div className="hero-body">
             <h2>Greetings, it&apos;s Salma.</h2>
             <p>
-              A <span className="role">Frontend Devloper</span> who loves to
+              A <span className="role">Frontend Developer</span> who loves to
               make blank pages turn into something great and user friendly!
             </p>
             
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,15 +5,13 @@ function Skills() {
     <div className="skills">
       <div className="skills-container">
         <div className="skills-wrapper">
-          <ul className="tech-skills">
-            <h2 className="skills-header">Skills</h2>
-            <div className="skill-container">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Responsive Design</li>
-            </div>
+          <h1>Techs <span>&</span> Skills</h1>
+          <ul className="techs">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>REACT</li>
+            <li>Responsive Design</li>
           </ul>
         </div>
       </div>
@@ -22,3 +20,19 @@ function Skills() {
 }
 
 export default Skills;
+
+/*<span>C</span>
+            <span>o</span>
+            <span>d</span>
+            <span>e</span> */
+
+/*<ul className="tech-skills">
+            <h2 className="skills-header">Skills</h2>
+            <div className="skill-container">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Responsive Design</li>
+            </div>
+          </ul> */
