cfb3f47bd1c64784290fd01638d0ab598499ed8f
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2023-11-26 10:59:26 +0200

    update projects section style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,9 @@ import "../styles/projects.css";
 import taskManagementApp from "../assets/projectsImages/taskManagement.jpg";
 import audiophilePic from "../assets/projectsImages/Audiophile-Pic.webp";*/
 
-import firstPic from "../assets/projectsImages/steve-johnson-bCME-_JwOHM-unsplash.jpg"
+import ecommerceWebsite from "../assets/projectsImages/Audiophile-Pic.webp";
+import archStudioPic from "../assets/projectsImages/audioStudioPic.webp";
+import taskManagementApp from "../assets/projectsImages/taskManagement.jpg";
 
 //audiophilePic
 //Audiophile-Pic.webp
@@ -30,82 +32,11 @@ function Projects() {
           that i&apos;ve done
         </p>
         <div className="projects-container">
-          <div className="project-container">
-            <img src={firstPic} />
-            <div className="project-info">
-              <h4 className="project-category">Business/Company Website</h4>
-              <h3 className="project-title">Arch Studio Website</h3>
-              <p className="techs-used">
-                Built with:{" "}
-                <span>
-                  <FontAwesomeIcon icon={faHtml5} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faCss3Alt} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faSquareJs} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faReact} style={style} />
-                </span>
-              </p>
-              <div className="links">
-                <a>
-                  View the code{" "}
-                  <span className="right-icon">
-                    <FontAwesomeIcon icon={faArrowRight} />
-                  </span>
-                </a>
-                <a>
-                  Visit the app{" "}
-                  <span className="right-icon">
-                    <FontAwesomeIcon icon={faArrowRight} />
-                  </span>
-                </a>
-              </div>
+          <div className="project-container" style={{alignSelf: "flex-start"}}>
+            <div className="project-img-container">
+              <img src={ecommerceWebsite} />
             </div>
-          </div>
 
-          {/*<div className="project-container">
-            <img src={taskManagementApp} />
-            <div className="project-info">
-              <h4 className="project-category">Utilities</h4>
-              <h3 className="project-title">Task Management App</h3>
-              <p className="techs-used">
-                Built with:{" "}
-                <span>
-                  <FontAwesomeIcon icon={faHtml5} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faCss3Alt} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faSquareJs} style={style} />
-                </span>{" "}
-                <span>
-                  <FontAwesomeIcon icon={faReact} style={style} />
-                </span>
-              </p>
-              <div className="links">
-                <a>
-                  View the code{" "}
-                  <span className="right-icon">
-                    <FontAwesomeIcon icon={faArrowRight} />
-                  </span>
-                </a>
-                <a>
-                  Visit the app{" "}
-                  <span className="right-icon">
-                    <FontAwesomeIcon icon={faArrowRight} />
-                  </span>
-                </a>
-              </div>
-            </div>
-  </div>*/}
-
-          {/*<div className="project-container">
-            <img src={audiophilePic} />
             <div className="project-info">
               <h4 className="project-category">E-Commerce Website</h4>
               <h3 className="project-title">Audiophile E-Commerce Website</h3>
@@ -139,8 +70,87 @@ function Projects() {
                 </a>
               </div>
             </div>
-          </div>*/}
+          </div>
 
+          <div className="project-container" style={{alignSelf: "flex-end"}}>
+            <div className="project-img-container">
+              <img src={taskManagementApp} />
+            </div>
+
+            <div className="project-info">
+              <h4 className="project-category">Utilities</h4>
+              <h3 className="project-title">Task Management App</h3>
+              <p className="techs-used">
+                Built with:{" "}
+                <span>
+                  <FontAwesomeIcon icon={faHtml5} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faCss3Alt} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faSquareJs} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faReact} style={style} />
+                </span>
+              </p>
+              <div className="links">
+                <a>
+                  View the code{" "}
+                  <span className="right-icon">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                </a>
+                <a>
+                  Visit the app{" "}
+                  <span className="right-icon">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-container" style={{alignSelf: "flex-start"}}>
+            <div className="project-img-container">
+              <img src={archStudioPic} />
+            </div>
+
+            <div className="project-info">
+              <h4 className="project-category">Business/Company Website </h4>
+              <h3 className="project-title">Arch Studio Website</h3>
+              <p className="techs-used">
+                Built with:{" "}
+                <span>
+                  <FontAwesomeIcon icon={faHtml5} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faCss3Alt} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faSquareJs} style={style} />
+                </span>{" "}
+                <span>
+                  <FontAwesomeIcon icon={faReact} style={style} />
+                </span>
+              </p>
+              <div className="links">
+                <a>
+                  View the code{" "}
+                  <span className="right-icon">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                </a>
+                <a>
+                  Visit the app{" "}
+                  <span className="right-icon">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
